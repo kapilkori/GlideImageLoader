e0e62a8cb5a563cb643e3585059486f6b41f2df9
Kapil Kori <kapil.kori@pardypanda.com>
2024-04-09 12:56:12 +0530

    Initial release

---
+++ app/src/main/java/com/test/myapplication/GlideImageGenerator.kt
@@ -0,0 +1,12 @@
+package com.test.myapplication
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+object GlideImageGenerator {
+
+    fun loadImage(context: Context, url: String, viewId: ImageView) {
+
+    }
+}
